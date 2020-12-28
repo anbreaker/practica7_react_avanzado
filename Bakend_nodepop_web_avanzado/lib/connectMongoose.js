6fbe3326d441fa9c9b8ddc01fa6e879f525fb2e6
anbreaker <antunez19@gmail.com>
2020-12-28 13:24:49 +0100

    inicio p7

---
+++ Bakend_nodepop_web_avanzado/lib/connectMongoose.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+
+mongoose.Promise = global.Promise;
+
+db.on('error', function (err) {
+  console.error('mongodb connection error:', err);
+  process.exit(1);
+});
+
+db.once('open', function () {
+  console.info('Connected to mongodb on', mongoose.connection.name);
+  console.info('Port', process.env.PORT);
+});
+
+mongoose.connect('mongodb://localhost/nodepop', {
+  useMongoClient: true,
+});
+
+module.exports = db;
